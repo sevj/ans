e0765ed31d820a36065f6a4f2fac18599560f277
Sevj <sevjeh@sevjeh.com>
2020-11-02 14:54:35 +0100

    Push

---
+++ ANSBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Adimeo\Notifications;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class ANSBundle
+ * @package Adimeo\Notifications
+ */
+class ANSBundle extends Bundle
+{
+
+}
