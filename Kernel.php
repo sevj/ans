6afc21e990ff19809fa83446f8b71f2e786a1c7b
Sevj <sevjeh@sevjeh.com>
2020-11-02 14:56:00 +0100

    Euh

--- Kernel.php
+++ Kernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
         ];
     }
 
+
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load(__DIR__ . '/config/ans.yml');
